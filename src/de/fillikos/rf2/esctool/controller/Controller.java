5b65817e48efe15daba418f394b61e673936aea8
fillikos <muench.oz@gmail.com>
2022-04-13 20:04:24 +0200

    Version 1.0.3-5

--- src/de/fillikos/rf2/esctool/controller/Controller.java
+++ src/de/fillikos/rf2/esctool/controller/Controller.java
@@ -16,7 +16,7 @@ public class Controller {
     private static final ArrayList<String> warnings = new ArrayList<>();
     private static final DataController dc = new DataController();
     private static final ViewController vc = new ViewController();
-    private static final String version = "1.0.2-4";
+    private static final String version = "1.0.3-5";
 
     public static void showView() {
         addLog("Controller.showView()");
